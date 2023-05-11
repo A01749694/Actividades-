764e829898863f52c133ab5e16e650e010407a77
A01749694 <117104224+A01749694@users.noreply.github.com>
2023-05-11 09:02:45 -0600

    Velocidad aumentada

--- juegotiro.py
+++ juegotiro.py
@@ -6,13 +6,13 @@ ball = vector(-200, -200)
 speed = vector(0, 0)
 targets = []
 
-def tap(x, y):
+def tap(x, y):  
     "Respond to screen tap."
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = 2*(x + 200) / 25  #Multiplicar con un factor para duplicar la velocidad  
+        speed.y = 2*(y + 200) / 25  
 
 def inside(xy):
     "Return True if xy within screen."
@@ -40,7 +40,7 @@ def move():
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        target.x -= 2.0  #Aumentar la velocidad de los objetivos a 2.0 
 
     if inside(ball):
         speed.y -= 0.35
